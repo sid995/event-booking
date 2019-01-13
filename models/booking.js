4c9ca0e6abf73a957018524d36ae7a4a2390c450
Siddharth Kundu <sk31395@gmail.com>
2019-01-14 00:44:39 +0530

    new booking model

---
+++ models/booking.js
@@ -0,0 +1,19 @@
+const mongoose = require('mongoose')
+
+const Schema = mongoose.Schema
+
+const bookingSchema = new Schema(
+  {
+    event: {
+      type: Schema.Types.ObjectId,
+      ref: 'Event'
+    },
+    user: {
+      type: Schema.Types.ObjectId,
+      ref: 'User'
+    }
+  },
+  { timeStamps: true }
+)
+
+module.exports = mongoose.model('Booking', bookingSchema)
