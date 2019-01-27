e2d4db62b6d7f93ee17bfaf16716756d6fd98523
Siddharth Kundu <sk31395@gmail.com>
2019-01-27 19:08:11 +0530

    fixed class name

--- frontend/src/components/Navigation/MainNavigation.js
+++ frontend/src/components/Navigation/MainNavigation.js
@@ -5,7 +5,7 @@ import './MainNavigation.css'
 const MainNavigation = props => {
   return (
     <header className="main-navigation">
-      <div className="main-naviation__logo">
+      <div className="main-navigation__logo">
         <h1>Event Booker</h1>
       </div>
       <nav className="main-navigation__items">
