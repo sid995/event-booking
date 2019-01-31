5c17a0b568edf1eca2a9f4f95945a7e143118f7a
Siddharth Kundu <sk31395@gmail.com>
2019-01-31 23:12:08 +0530

    Added VookingList component

--- frontend/src/pages/Bookings.js
+++ frontend/src/pages/Bookings.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react'
+import BookingList from '../components/Bookings/BookingList/BookingList'
 import Spinner from '../components/Spinner/Spinner'
 import AuthContext from '../context/auth-context'
 
@@ -56,22 +57,57 @@ class BookingsPage extends Component {
       })
   }
 
+  deleteBookingHandler = bookingId => {
+    this.setState({ isLoading: true })
+    const requestBody = {
+      query: `
+        mutation {
+          cancelBooking(bookingId: "${bookingId}"){
+            _id
+            title
+          }
+        }
+      `
+    }
+
+    fetch('http://localhost:8800/graphql', {
+      method: 'POST',
+      body: JSON.stringify(requestBody),
+      headers: {
+        'Content-Type': 'application/json',
+        Authorization: 'Bearer ' + this.context.token
+      }
+    })
+      .then(res => {
+        if (res.status !== 200 && res.status !== 201) {
+          throw new Error('Failed')
+        }
+        return res.json()
+      })
+      .then(resData => {
+        this.setState(prevState => {
+          const updatedBookings = prevState.bookings.filter(booking => {
+            return booking._id !== bookingId
+          })
+          return { bookings: updatedBookings, isLoading: false }
+        })
+      })
+      .catch(err => {
+        console.log(err)
+        this.setState({ isLoading: false })
+      })
+  }
+
   render() {
     return (
       <React.Fragment>
         {this.state.isLoading ? (
           <Spinner />
         ) : (
-          <ul>
-            {this.state.bookings.map(booking => {
-              return (
-                <li key={booking._id}>
-                  {booking.event.title} -{' '}
-                  {new Date(booking.createdAt).toLocaleDateString()}
-                </li>
-              )
-            })}
-          </ul>
+          <BookingList
+            bookings={this.state.bookings}
+            onDelete={this.deleteBookingHandler}
+          />
         )}
       </React.Fragment>
     )
